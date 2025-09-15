95fef4dbe7952078583f04d899f928359661a750
Ahmir Postell <postell.ahmir@gmail.com>
2025-09-15 13:36:42 -0400

    handled warnings

--- src/modules/ui/tui/ui_loop.v
+++ src/modules/ui/tui/ui_loop.v
@@ -33,8 +33,6 @@ fn ui_loop(x voidptr) {
 			wrap_points = [0] // guarantee empty lines draw
 		}
 
-		visual_indices := buf.visual_col[logical_idx]
-
 		mut runes := line.runes()
 		runes << ` ` // ensure empty lines render
 
@@ -72,13 +70,13 @@ fn ui_loop(x voidptr) {
 			// draw characters for this wrap row
 			start := wrap_points[wrap_row]
 			end := if wrap_row + 1 < wrap_points.len { wrap_points[wrap_row + 1] } else { runes.len }
-			mut segment := runes[start..end]
+			mut segment := runes[start..end].clone()
 
 			segment << ` `
 
 			mut visual_col := 0
 
-			for j, ch in segment {
+			for _, ch in segment {
 				mut printed := ch
 				mut char_width := 1
 				if ch == `\t` {
@@ -114,86 +112,6 @@ fn ui_loop(x voidptr) {
 		// move to next buffer line
 		logical_idx++
 	}
-
-	// // initialize variables to simplify loop
-	// start_row := app.viewport.row_offset
-	// end_row := math.min(app.buffer.lines.len, app.viewport.row_offset + app.viewport.height)
-	// col_start := app.viewport.col_offset + app.viewport.line_num_to_text_gap
-
-	// mut wrap_offset := 0
-	// mut new_row := 0
-	// mut wrap_points := []int{}
-	// mut new_col := 0
-
-	// // render all visual lines
-	// render all tabs as 4 spaces
-	// for i, line in app.buffer.lines[start_row..end_row] {
-	// 	line_num := i + 1
-	// 	alignment_spaces := ' '.repeat(app.buffer.lines.len.str().len - line_num.str().len)
-	// 	wrap_points = app.viewport.build_wrap_points(line)
-
-	// draw line number
-	// if i == app.visual_cursor.y {
-	// 	for row in 0 .. wrap_points.len {
-	// 		ctx.set_bg_color(app.viewport.line_bg_color)
-	// 		ctx.set_color(app.viewport.active_line_number_color)
-	// 		ctx.draw_line(0, i + 1 + row, width - 1, i + 1 + row)
-	// 	}
-	// 	ctx.draw_text(app.viewport.col_offset, line_num, alignment_spaces + line_num.str())
-	// 	ctx.reset_bg_color()
-	// 	ctx.reset_color()
-	// } else {
-	// 	ctx.set_color(app.viewport.inactive_line_number_color)
-	// 	ctx.draw_text(app.viewport.col_offset, line_num, alignment_spaces + line_num.str())
-	// 	ctx.reset_color()
-	// }
-	// visual_line := app.buffer.visual_col[start_row + i]
-	// mut runes := line.runes()
-	// runes << ` `
-	// for j, ch in runes {
-	// 	// visual column from cache
-	// 	mut col := if j < visual_line.len {
-	// 		visual_line[j]
-	// 	} else {
-	// 		if visual_line.len > 0 { visual_line.last() + 1 } else { 0 }
-	// 	}
-
-	// new_row = 0
-	// new_col = col
-	// for index := 1; index < wrap_points.len && wrap_points[index] <= col; index++ {
-	// 	new_col = (col + app.viewport.width) % app.viewport.width
-	// 	new_row = index
-	// }
-	// mut printed := ch
-	// mut char_width := 1
-	// if ch == `\t` {
-	// 	printed = ` `
-	// 	char_width = app.buffer.tabsize
-	// }
-
-	// draw text
-	// for k in 0 .. char_width {
-	// 	let_draw_y := wrap_offset + new_row + 1
-	// 	let_draw_x := col_start + new_col + k + 1
-	// 	if col == app.visual_cursor.x && i == app.visual_cursor.y {
-	// 		ctx.set_bg_color(app.visual_cursor.color)
-
-	// 		ctx.set_color(app.visual_cursor.text_color)
-	// 		ctx.draw_text(let_draw_x, let_draw_y, printed.str())
-	// 		ctx.reset_bg_color()
-	// 		ctx.reset_color()
-	// 	} else if i == app.visual_cursor.y {
-	// 		ctx.set_bg_color(app.viewport.line_bg_color)
-	// 		ctx.draw_text(let_draw_x, let_draw_y, printed.str())
-	// 		ctx.reset_bg_color()
-	// 	} else {
-	// 		ctx.draw_text(let_draw_x, let_draw_y, printed.str())
-	// 	}
-	// }
-	// 	}
-
-	// 	wrap_offset += math.max(1, wrap_points.len - 1)
-	// }
 
 	// ctx.horizontal_separator(height - 2)
 	ctx.set_bg_color(constants.deep_indigo)
